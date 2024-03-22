5d3b3e27a512c89f9b14e17bd2fd28258e50046c
Kenneth <kennethyoung.2960@gmail.com>
2024-03-22 01:06:51 -0500

    Final commit

--- Modern_asynchronous_programming_Promise_chaining_Node_18_1/src/main.js
+++ Modern_asynchronous_programming_Promise_chaining_Node_18_1/src/main.js
@@ -3,16 +3,11 @@ const BASE_URL = "http://localhost:5000";
 
 function updateIfExists(id, body) {
   const url = `${BASE_URL}/constellations/${id}`;
-  return axios
-    .get(url)
-    .then(() => {
-      axios.put(url, body).then(({ data }) => {
-        console.log(data);
-      });
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
+  
+  return axios.get(url)
+    .then(() => axios.put(url, body))
+    .then(({ data }) => data)
+    .catch(error => error.message);
 }
 
 module.exports = {
